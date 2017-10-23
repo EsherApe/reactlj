b02c06cddf5ea3c5d474dfbfca240dea2464f9f3
funkybo <esher5580@gmail.com>
2017-10-23 12:42:04 +0300

    article list

--- src/Article.js
+++ src/Article.js
@@ -4,7 +4,7 @@ export default class Article extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isOpen: false
+            isOpen: true
         }
     }
 
