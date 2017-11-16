071c17bcfb09f56486b9ab99b0c0ac991a3984d9
funkybo <esher5580@gmail.com>
2017-11-16 15:28:50 +0200

    add logger middleware

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,12 +1,18 @@
 import {createSelector} from 'reselect';
 const articlesGetter = state => state.articles;
 const filtersGetter = state => state.filters;
+const commentsGetter = state => state.comments;
+const idGetter = (state, props) => props.id;
 
 export const filtrateArticlesSelector = createSelector(articlesGetter, filtersGetter, (articles, filters) => {
     const {selected, dateRange: {from, to}} = filters;
-    console.log('computing filtration');
+
     return articles.filter(article => {
         const published = Date.parse(article.date);
         return (!selected.length || selected.includes(article.id)) && (!from || !to || (published > from && published < to))
     });
+});
+
+export const commentSelectorFactory = () => createSelector(commentsGetter, idGetter, (comments, id) => {
+    return comments[id];
 });
